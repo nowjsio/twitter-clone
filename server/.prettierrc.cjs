cb1be0548b148609caa9a5404486606068fe8532
nowjsio <nowjsio@gmail.com>
2021-12-20 03:05:28 +0900

    Style: modify .json and prettier config
    (cherry picked from commit 218221630a966c6b9d183b7ecc3b734fb450511d)

--- server/.prettierrc.cjs
+++ server/.prettierrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   semi: true, // 코드 뒤에 세미콜론(;) 사용
   useTabs: false, // tap키 사용하지 않음
   tabWidth: 2, // 들여쓰기 2칸 사용
-  // trailingComma: 'none',      // 후행쉼표 , ex) 1,2,3, <-
+  trailingComma: 'all', // 후행쉼표 , ex) 1,2,3, <-
   printWidth: 80, // 한 줄은 80칸이 넘지 않도록 함
-  arrowParens: 'always', // 화살표 함수 괄호 사용 방식
+  arrowParens: 'avoid', // 하나의 argument 를 받을 때, 괄호를 생략함
 };
